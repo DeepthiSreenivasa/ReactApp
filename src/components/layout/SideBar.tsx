8403bd42379293322625815e2f8e6c226c9f3f65
BsMurthy <murthy@hoshitoolshp.com>
2026-08-18 16:54:03 +0530

    Adding side bar

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,5 +1,39 @@
+import { NavLink } from 'react-router-dom';
+import './SideBar.css';
+
 const SideBar = () => {
-    return <h1>This is SideBar</h1>
-}
+  const sideBarItems = [
+    {
+      id: 'dashboard',
+      label: 'Dashboard',
+      path: '/dashboard',
+    },
+    {
+      id: 'markets',
+      label: 'Markets',
+      path: '/markets',
+    },
+    {
+      id: 'watchlist',
+      label: 'Watchlist',
+      path: '/watchlist',
+    },
+  ];
+
+  return (
+    <>
+      {sideBarItems.map((item) => (
+        <div key={item.id}>
+          <NavLink
+            to={item.path}
+            className={({ isActive }) => (isActive ? 'active' : '')} //--> You need not create a state ,rather re use this
+          >
+            {item.label}
+          </NavLink>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default SideBar;
